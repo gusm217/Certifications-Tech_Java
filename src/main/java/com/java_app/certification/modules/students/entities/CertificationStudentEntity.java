da0a8d1e67c7bee1072be4bcaa037bc5ae86ff17
Gustavo Mendes <gusm217@gmail.com>
2024-07-01 16:31:49 -0300

    feat: finish entities relations

--- src/main/java/com/java_app/certification/modules/students/entities/CertificationStudentEntity.java
+++ src/main/java/com/java_app/certification/modules/students/entities/CertificationStudentEntity.java
@@ -1,7 +1,10 @@
 package com.java_app.certification.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +29,7 @@ public class CertificationStudentEntity {
 	private UUID id;
 
 	@Column(length = 100)
-	private String techonology;
+	private String technology;
 
 	@Column(length = 10)
 	private int grade;
@@ -37,5 +41,10 @@ public class CertificationStudentEntity {
 	@JoinColumn(name = "student_id", insertable = false, updatable = false)
 	private StudentEntity studentEntity;
 
-	// List<AnswersCertificationsEntity> answersCertificationEntity;
+	@OneToMany
+	@JoinColumn(name = "answer_certification_id", insertable = false, updatable = false)
+	List<AnswersCertificationsEntity> answersCertificationEntity;
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
 }
--- src/main/java/com/java_app/certification/modules/students/entities/StudentEntity.java
+++ src/main/java/com/java_app/certification/modules/students/entities/StudentEntity.java
@@ -1,7 +1,10 @@
 package com.java_app.certification.modules.students.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -28,5 +31,8 @@ public class StudentEntity {
 
 	@OneToMany(mappedBy = "studentEntity")
 	private List<CertificationStudentEntity> certificationStudentEntity;
+
+	@CreationTimestamp
+	private LocalDateTime createdAt;
 
 }
