24d90a8501da14e575a39baed99e8428788f56ce
Gustavo Mendes <gusm217@gmail.com>
2024-07-01 16:32:13 -0300

    feat: add new query to useCase

--- src/main/java/com/java_app/certification/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/java_app/certification/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,20 @@
 package com.java_app.certification.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.java_app.certification.modules.students.dto.VerifyIfHasCertificationDTO;
+import com.java_app.certification.modules.students.repositories.CertificationStudentRepository;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+	@Autowired
+	private CertificationStudentRepository certificationStudentRepository;
+
 	public boolean execute(VerifyIfHasCertificationDTO dto) {
-		if (dto.getEmail().equals("teste@teste.com") && dto.getTechnology().equals("JAVA")) {
+		var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+		if (!result.isEmpty()) {
 			return true;
 		}
 
